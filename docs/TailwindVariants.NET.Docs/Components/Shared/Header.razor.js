b9c646538247c06dddc34842d171c315ad5ac61f
Denny09310 <k.denny2000@gmail.com>
2025-10-03 09:16:45 +0200

    fix: javascript function was not bound

--- docs/TailwindVariants.NET.Docs/Components/Shared/Header.razor.js
+++ docs/TailwindVariants.NET.Docs/Components/Shared/Header.razor.js
@@ -1,4 +1,7 @@
 ﻿export default class extends BlazorJSComponents.Component {
+    attach() {
+        this.apply = this.apply.bind(this);
+    }
     setParameters({ header }) {
         this.header = header;
         this.apply();
@@ -10,5 +13,5 @@
         } else {
             this.header.classList.remove('backdrop-blur', 'border-b', 'border-gray-200', 'bg-gray-50/70', 'dark:bg-neutral-900/70', 'dark:border-neutral-700');
         }
-    };
+    }
 }
