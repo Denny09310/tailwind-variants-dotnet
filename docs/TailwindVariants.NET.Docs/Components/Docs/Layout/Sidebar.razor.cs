c8fc87ecb649fdad47e4f5449e809654962ed724
Denny09310 <k.denny2000@gmail.com>
2025-10-04 09:12:41 +0200

    feat: added sidebar header, fixed styles

--- docs/TailwindVariants.NET.Docs/Components/Docs/Layout/Sidebar.razor.cs
+++ docs/TailwindVariants.NET.Docs/Components/Docs/Layout/Sidebar.razor.cs
@@ -4,14 +4,15 @@ namespace TailwindVariants.NET.Docs.Components.Docs.Layout
     {
         private static readonly TvDescriptor<Sidebar, Slots> _sidebar = new
         (
-            @base: "fixed top-0 left-0 h-full w-64 z-50 transform transition-transform duration-300 bg-white dark:bg-neutral-800 md:h-auto md:sticky md:top-16 md:block md:self-start md:bg-transparent!",
+            @base: "fixed top-0 left-0 h-full w-64 z-50 transform transition-transform duration-300 bg-white dark:bg-neutral-900 md:h-auto md:sticky md:top-16 md:block md:self-start md:bg-transparent!",
             slots: new()
             {
-                [s => s.Overlay] = "transition-colors",
-                [s => s.Wrapper] = "p-4 mt-3 flex flex-col gap-1 overflow-y-auto md:mt-0",
-                [s => s.Header] = "mt-3 font-semibold text-neutral-600 dark:text-neutral-300",
-                [s => s.Link] = "rounded px-3 py-2 text-sm hover:bg-sky-50 dark:hover:bg-sky-700/50",
-                [s => s.ActiveLinks] = "bg-slate-50! dark:bg-sky-700/50!",
+                [s => s.Overlay] = "transition-[opacity]",
+                [s => s.Wrapper] = "p-4 flex flex-col gap-1 overflow-y-auto",
+                [s => s.Header] = "flex items-center justify-between p-4 border-b border-neutral-200 dark:border-neutral-700 md:hidden",
+                [s => s.Link] = "rounded px-3 py-2 text-sm hover:bg-sky-200/50 dark:hover:bg-sky-700/50",
+                [s => s.LinkHeader] = "mt-3 font-semibold text-neutral-600 dark:text-neutral-300",
+                [s => s.LinkActive] = "bg-sky-200/50! dark:bg-sky-700/50!",
             },
             variants: new()
             {
@@ -35,11 +36,12 @@ namespace TailwindVariants.NET.Docs.Components.Docs.Layout
 
         public sealed partial class Slots : ISlots
         {
-            public string? ActiveLinks { get; set; }
             public string? Base { get; set; }
-            public string? Link { get; set; }
-            public string? Overlay { get; set; }
             public string? Header { get; set; }
+            public string? Link { get; set; }
+            public string? LinkActive { get; set; }
+            public string? LinkHeader { get; set; }
+            public string? Overlay { get; set; }
             public string? Wrapper { get; set; }
         }
     }
